94de7b6fd3cdcec0b6d0c6019e0ac557bbe937f2
negarkakavan <negarkakavan@gmail.com>
2024-03-01 01:50:43 +0330

    Header

--- Project1/Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Project1/Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c571a5743ba639800264049ac0fc71d9dcdb6f1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
